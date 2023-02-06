d41e56710fbfabc700af7e12be6a590c7354d3bf
Ben Greig <aquillo@bigpond.net.au>
2023-02-07 10:05:10 +1100

    project model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
+  factory :project do
+    title { "MyString" }
+    due_date { "2023-02-07" }
+    description { "MyText" }
+    status { 1 }
+  end
+
   factory(:user) do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
